728a55d90b641cdd5ccbff919c264e7bd8e41977
blackman147 <chuksntoka@gmail.com>
2022-10-04 09:38:20 +0100

    r progrmming

---
+++ LAB01.R
@@ -0,0 +1,84 @@
+#EXAMPLE
+X <- 1:5
+X
+
+#TASK 1
+
+X<- 5
+
+#task2
+X
+
+#task 3
+y<- c(1,2,3,4,5)
+y
+
+x <- seq(1, 5, 1)
+x
+
+#task 4
+y <- 2 * x+1
+y
+
+#task 5
+
+temp <- x>13
+temp
+
+#task 6
+
+a<-c(0.5, 0.6)
+a
+
+a<-c(TRUE, FALSE)
+a
+
+a<-c(T,F)
+a
+
+a <- c("a","b","c")
+a
+
+a <- 9:29
+a
+
+a<- c(1+0i, 2+4i)
+a
+
+#task 7
+
+z <- c(1, 3, NA)
+z
+
+is.na(z)
+
+#task 8
+is.nan(z)
+
+#task 9
+help(mean)
+
+#task 11
+x <- 1:5
+rep(x, 5)
+
+#task 12
+
+help(rep)
+rep(x, 5, each = 3)
+
+#task 13
+
+help(scan)
+w <- scan()
+w
+
+#task 14
+A <- array(1:25, dim = c(5,5))
+A
+
+#task 15
+A[1,4]
+A[1, 1:3]
+A[1:3, 1:3]
+A[c(1,3,5), 2]
